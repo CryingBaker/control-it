5b7338c4a7561eb127ff57bd94acc484947c6117
Atharv <107851113+CryingBaker@users.noreply.github.com>
2024-09-05 07:25:30 +0530

    Update firebaseConfig.js

--- app/app/firebaseConfig.js
+++ app/app/firebaseConfig.js
@@ -2,14 +2,14 @@
 // import { getAnalytics } from "firebase/analytics";
 
 // const firebaseConfig = {
-//   apiKey: "AIzaSyCAaxPFyXW_iOozRUaAGelVp5DjWRxI3GA",
-//   authDomain: "control-it-38c7d.firebaseapp.com",
-//   databaseURL: "https://control-it-38c7d-default-rtdb.asia-southeast1.firebasedatabase.app",
-//   projectId: "control-it-38c7d",
-//   storageBucket: "control-it-38c7d.appspot.com",
-//   messagingSenderId: "79349936132",
-//   appId: "1:79349936132:web:1bede204fc1abbf5044551",
-//   measurementId: "G-FX4514P0P9"
+//   apiKey: "",
+//   authDomain: "",
+//   databaseURL: "",
+//   projectId: "",
+//   storageBucket: "",
+//   messagingSenderId: "",
+//   appId: "",
+//   measurementId: ""
 // };
 
 // const app = initializeApp(firebaseConfig);
